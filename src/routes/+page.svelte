3dc5e4546cd3be9941202961d3cc4bb5ba4fc94b
SophiWeiss <owlety34@gmail.com>
2024-03-09 20:31:41 +0100

    basic layout (grid)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to Uika Website</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="whole-main-page">
+  <div class="navigation-bar">
+    <h1>Welcome to Uika Website</h1>
+  </div>
+  <div class="main-boxes">
+    <div class="box1">One</div>
+    <div class="box2">Two</div>
+    <div class="box3">Three</div>
+    <div class="box4">Four</div>
+    <div class="box5">Five</div>
+    <div class="box6">Six</div>
+  </div>
+</div>
+
+<style>
+  .main-boxes {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-auto-rows: 100px;
+  }
+
+  .box1 {
+    grid-column-start: 1;
+    grid-column-end: 4;
+    grid-row-start: 1;
+    grid-row-end: 3;
+  }
+
+  .box2 {
+    grid-column-start: 1;
+    grid-row-start: 3;
+    grid-row-end: 5;
+  }
+</style>
