c0f62f262f0df2df109246f8dc8fca03216e93e9
SophiWeiss <owlety34@gmail.com>
2024-03-12 10:46:35 +0100

    added separate file for strip

---
+++ src/routes/Strip.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  export let length: number = 10
+</script>
+
+<div>
+  {Array.from({ length })
+    .map(() => 'Comming Soon')
+    .join(' • ')}
+</div>
+
+<style>
+  div {
+    display: block;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) rotate(-30deg);
+    background-color: var(--color-bg-accent-variant);
+    color: var(--color-text-accent-variant);
+    text-align: center;
+    padding: 5px 0;
+    font-weight: bold;
+    overflow: hidden;
+    white-space: nowrap;
+    pointer-events: none;
+  }
+</style>
