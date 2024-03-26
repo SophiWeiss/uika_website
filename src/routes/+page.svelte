464732f48781d089fa39824de93b2f0827c52474
SophiWeiss <owlety34@gmail.com>
2024-03-26 10:01:53 +0200

    added all

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,61 +1,76 @@
-<div class="whole-main-page">
-  <div class="navigation-bar">
+<script lang="ts">
+  import { flip } from 'svelte/animate'
+  import { dndzone, type DndEvent } from 'svelte-dnd-action'
+  import CommingSoon from './CommingSoon.svelte'
+  import Box1 from './Box1.svelte'
+  import type { ComponentType } from 'svelte'
+
+  function elasticOut(stifness: number, damping: number) {
+    return (t: number) => {
+      const D = Math.sqrt(stifness - Math.pow(damping, 2))
+      return (
+        1 +
+        Math.pow(1 - Math.sqrt(t), damping) * ((damping / D) * Math.sin(D * t) - Math.cos(D * t))
+      )
+    }
+  }
+
+  type Box = {
+    id: number
+    component: ComponentType
+  }
+
+  let boxes: Box[] = [
+    { id: 1, component: Box1 },
+    { id: 2, component: CommingSoon },
+    { id: 3, component: CommingSoon },
+    { id: 4, component: CommingSoon },
+    { id: 5, component: CommingSoon },
+    { id: 6, component: CommingSoon }
+  ]
+
+  function handleDnd(e: CustomEvent<DndEvent<Box>>) {
+    boxes = e.detail.items
+  }
+</script>
+
+<main>
+  <nav>
     <h1>Welcome to Uika Website</h1>
+  </nav>
+  <div
+    use:dndzone={{ items: boxes, flipDurationMs: 300 }}
+    on:consider={handleDnd}
+    on:finalize={handleDnd}
+    class="boxes"
+  >
+    {#each boxes as box (box.id)}
+      <div
+        animate:flip={{ duration: d => Math.sqrt(d) * 600, easing: elasticOut(500, 9) }}
+        class="box"
+      >
+        <svelte:component this={box.component} />
+      </div>
+    {/each}
   </div>
-  <div class="main-boxes">
-    <div class="box1 box"></div>
-    <div class="box2 box"></div>
-    <div class="box3 box"></div>
-    <div class="box4 box"></div>
-    <div class="box5 box"></div>
-    <div class="box6 box"></div>
-  </div>
-</div>
+</main>
 
 <style>
-  .main-boxes {
+  .boxes {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     grid-template-rows: repeat(2, 1fr);
     height: 100vh;
-    background-color: #fff2fb;
+    background-color: var(--color-red);
     gap: 20px;
     position: relative;
     padding: 20px;
   }
-  :global(body) {
-    background-color: #fff2fb;
-  }
-  :global(h1) {
-    color: #3c3666;
-    text-align: center;
-    vertical-align: center;
-    margin-top: 50px;
-    margin-bottom: 50px;
-  }
+
   .box {
     border-radius: 20px;
-    padding: 20px;
-    background-color: #817ca9;
+    background-color: var(--color-bg-card);
     position: relative;
     overflow: hidden;
-  }
-  .box::after {
-    content: 'Coming soon • Coming soon • Coming soon	• Coming soon	• Coming soon	• Coming soon	• Coming soon	• Coming soon	• Coming soon	• Coming soon'; /* Текст повторяется, чтобы заполнить полосу */
-    display: block;
-    position: absolute;
-    top: 80%;
-    left: 0;
-    width: 200%;
-    transform: translate(-5%, -5%) rotate(-30deg);
-    transform-origin: 0 0;
-    background-color: #ebbb5d;
-    color: #ffffff;
-    text-align: center;
-    padding: 5px 0;
-    font-weight: bold;
-    overflow: hidden;
-    white-space: nowrap;
-    pointer-events: none;
   }
 </style>
